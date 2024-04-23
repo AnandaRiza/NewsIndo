7ba1119c0fe8fda5ebb4251ac3aae5376e560ce6
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-04-23 09:54:41 +0700

    Adding model and API Services

---
+++ NewsIndo/services/APIService.swift
@@ -0,0 +1,36 @@
+//
+//  APIService.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    static let shared = APIService()
+    private init() {
+        
+    }
+    
+    
+    func fetchNews() async throws -> [NewsArticle]{
+        guard let url = URL(string: Constant.newsURL) else { throw
+            URLError(.badURL)}
+        
+        let news = try await withCheckedThrowingContinuation { continuation in
+            AF.request(url).responseDecodable(of: News.self) { response in
+                switch response.result {
+                case .success(let newsResponse):
+                    continuation.resume(returning: newsResponse.data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        
+        
+        return news
+    }
+}
---
+++ NewsIndo/services/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+
+struct Constant {
+    static let newsURL = "https://berita-indo-api-next.vercel.app/api/kumparan-news/"
+    
+    static let newsTitle = "Berita Indo"
+    
+}
