3ea929a62b960b93a4505eb6a8bb33695f8722a6
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-04-23 11:16:25 +0700

    Safari Services

---
+++ NewsIndo/viewmodel/NewsVM.swift
@@ -0,0 +1,31 @@
+//
+//  NewsVM.swift
+//  NewsIndo
+//
+//  Created by MACBOOK PRO on 23/04/24.
+//
+
+import Foundation
+
+
+@MainActor
+class NewsVM: ObservableObject{
+    @Published var articles = [NewsArticle]()
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    func fetchNews() async {
+        isLoading = true
+        errorMessage = nil
+        
+        do{
+            articles = try await APIService.shared.fetchNews()
+            isLoading = false
+            
+        }catch{
+            errorMessage = "🙅‍♀️ \(error.localizedDescription). Failed to fetch News from API 🙅‍♀️ "
+            print(errorMessage ?? "N/A")
+            isLoading = false
+        }
+    }
+}
